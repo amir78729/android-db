5d37c7ee21e898d84f4da0ed96a21201a95bd8ac
Amirhossein <amir78729@aut.ac.ir>
2019-09-05 16:00:32 +0430

    db addDiseases() method

--- app/src/main/java/com/example/diseases/data/DatabaseHandler.java
+++ app/src/main/java/com/example/diseases/data/DatabaseHandler.java
@@ -1,10 +1,12 @@
 package com.example.diseases.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.diseases.R;
+import com.example.diseases.model.Disease;
 import com.example.diseases.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -26,7 +28,18 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
+    }
 
+    public void addDiseases(Disease disease){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, disease.getName());
+        values.put(Util.KEY_DESCRIPTION, disease.getDescriptiom());
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close();
     }
 }
