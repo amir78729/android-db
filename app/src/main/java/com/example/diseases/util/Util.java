a5992443c275122ccc2b15b88e20de3b2ca0f0f8
Amirhossein <amir78729@aut.ac.ir>
2019-09-05 15:17:37 +0430

    util class created

---
+++ app/src/main/java/com/example/diseases/util/Util.java
@@ -0,0 +1,13 @@
+package com.example.diseases.util;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "diseases_db";
+    public static final String TABLE_NAME = "diseases";
+
+    //columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DESCRIPTION = "description";
+
+}
